fe442478fc397a3e3bebc935523afcce8c5aa719
Reticulmz <36363244+Reticulmz@users.noreply.github.com>
2026-01-04 11:14:00 +0000

    Add Nix Configuration Files

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Home Manager configuration of reticulmz";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/nixos-wsl";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, nixos-wsl, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations."reticulmz" = nixpkgs.lib.nixosSystem {
+	inherit system;
+
+	modules = [
+	  ./nixos/configuration.nix
+	  nixos-wsl.nixosModules.wsl
+	];
+      };
+      homeConfigurations."reticulmz" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home-manager/home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
