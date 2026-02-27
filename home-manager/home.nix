223ac95ad039523e8b5d68a84712f59ffe6a670c
Reticulmz <36363244+Reticulmz@users.noreply.github.com>
2026-02-27 17:24:46 +0000

    unfreeを通すように変更

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
 {
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+  };
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "reticulmz";
